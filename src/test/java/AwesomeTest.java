7d20de792bc7012ce2c795d0f81e3c99e0a918d0
Mhkngithub <greeklenovo@outlook.com>
2020-10-06 22:59:14 +0300

    added more awesome steps

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -1,2 +1,8 @@
 public class AwesomeTest {
+
+    // ı just pull the new change from github that ı did not have locally
+    //and  now my project is in  sync with remote repository
+
+    // ı made more awesome changes
+
 }
